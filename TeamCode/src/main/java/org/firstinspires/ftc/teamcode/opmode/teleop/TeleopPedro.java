20341309b4c14925c2fa481743e5661b9196ba6d
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-04-15 08:48:51 -0400

    swdfgv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleopPedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleopPedro.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.BotWithPedro;
 
 @Config
-@TeleOp(name = "PedroTeleop", group = "Linear OpMode")
+@TeleOp(name = "TeleopPedro", group = "Linear OpMode")
 public class TeleopPedro extends LinearOpMode {
     private BotWithPedro bot;
 
