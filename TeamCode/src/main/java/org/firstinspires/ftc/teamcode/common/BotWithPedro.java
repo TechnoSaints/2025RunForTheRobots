d08afcdf6c2be150696a8b210c8bd8aa85a78f0f
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-05-01 11:18:30 -0400

    Update BotWithPedro.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/BotWithPedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/BotWithPedro.java
@@ -16,11 +16,15 @@ public class BotWithPedro extends Bot {
     private final Follower follower;
     private final double maxSlowPower = DrivetrainData.maxSlowTeleopPower;
     private final double maxFastPower = DrivetrainData.maxFastTeleopPower;
+
+    private boolean teleop = true;
+
     public BotWithPedro(OpMode opMode, Telemetry telemetry) {
         super(opMode, telemetry);
         follower = new Follower(opMode.hardwareMap, FConstants.class, LConstants.class);
         follower.setStartingPose(FieldLocations.startPose);
         follower.startTeleopDrive();
+        teleop = true;
     }
 
     public void followPath(PathChain path, boolean holdEnd) {
@@ -57,15 +61,22 @@ public class BotWithPedro extends Bot {
         } else {
             if ((Math.abs(-gamepad.left_stick_y) < 0.2) && (Math.abs(-gamepad.left_stick_x) < 0.2) && (Math.abs(-gamepad.right_stick_x) < 0.2)) {
                 follower.holdPoint(follower.getPose());
+                teleop = false;
             } else {
                 moveTeleop(-gamepad.left_stick_y * maxFastPower, -gamepad.left_stick_x * maxFastPower, -gamepad.right_stick_x * maxFastPower);
             }
+        }
+
+        if (!follower.isBusy() && !teleop)
+        {
+            teleop = true;
+            follower.startTeleopDrive();
         }
     }
 
     private void moveTeleop(double forward, double lateral, double heading)
     {
-        follower.startTeleopDrive();
+//        follower.startTeleopDrive();
         follower.setTeleOpMovementVectors(forward, lateral, heading, true);
     }
 
