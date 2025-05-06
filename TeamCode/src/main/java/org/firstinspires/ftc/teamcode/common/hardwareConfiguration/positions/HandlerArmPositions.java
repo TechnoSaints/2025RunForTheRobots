7ba7bf1fe7db03f407be58f801c6618d7e7ea65c
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-05-06 10:50:48 -0400

    Positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/positions/HandlerArmPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/positions/HandlerArmPositions.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.common.hardwareConfiguration.positions;
 
 public enum HandlerArmPositions {
-    RETRACTED(1.9375),
-    EXTENDED(23.5);
+    SPECIMEN_WALL(0.1),
+    HIGH_BASKET(.33),
+    HANDOFF(.78),
+    SPECIMEN_HANG(0.65);
     private double value;
     HandlerArmPositions(double value) {
         this.value = value;
