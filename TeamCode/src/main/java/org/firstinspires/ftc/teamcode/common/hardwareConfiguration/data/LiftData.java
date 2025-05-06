ab81a35211697cd7f1c81b103801e2e2d82a0ab6
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-05-06 13:56:58 -0400

    Update LiftData.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/data/LiftData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/data/LiftData.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data;
 
 public class LiftData {
-    public double maxMovePower = 0.9;
-    public double stopPower = 0.95;
-    public double lockPower = 0.75;
+    public double maxMovePower = 0.75;
+    public double stopPower = 0.80;
+    public double lockPower = 0.70;
     public int tolerance = 25;
 }
 
