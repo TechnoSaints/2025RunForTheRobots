07202bc9343404ebb322fb91508ad1b351eaf36a
Henderson <hendersonc@trinityprep.org>
2025-05-10 12:19:21 -0400

    wrfed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/data/ExtendoData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardwareConfiguration/data/ExtendoData.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class ExtendoData {
-    public double minLengthInches = 1.9375;
+    public double minLengthInches = 3.375;
     public double minLengthTicks = 0.45;
-    public double maxLengthInches = 23.5;
+    public double maxLengthInches = 24.1875;
     public double maxLengthTicks = 0.75;
-    public double nearLinkageLengthInches = 11.6875;
+    public double nearLinkageLengthInches = 11.75;
     public double farLinkageLengthInches = 12.625;
 }
 
