35f00c2021fd7bae3403c20a980275ec70f75ce3
zigathustra <130092700+TechnoSaints@users.noreply.github.com>
2025-05-27 11:22:29 -0400

    sdv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/vision/VisionSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/vision/VisionSensor.java
@@ -9,12 +9,12 @@ import org.firstinspires.ftc.teamcode.common.Component;
 abstract public class VisionSensor extends Component {
     protected double targetHeightInches, verticalHeightInches;          // Distance from the ground for calculation
     protected double totalVerticalAngleDegrees, totalHorizontalAngleDegrees;
-    protected double forwardDistanceToTargetInches, leftDistanceToTargetInches;
+    protected double forwardDistanceToTargetInches, leftDistanceToTargetInches, leftRotationToTargetDegrees;
     protected double forwardMinInches;
     protected double forwardMaxInches;
     protected double leftMaxInches;
     protected double leftMinInches;
-    protected ElapsedTime timer = new ElapsedTime();
+    protected final ElapsedTime timer = new ElapsedTime();
     protected double grabberLeftOffsetInches;
     protected double grabberForwardOffsetInches;
 
@@ -38,6 +38,10 @@ abstract public class VisionSensor extends Component {
         return leftDistanceToTargetInches;
     }
 
+    public double getLeftRotationToTargetDegrees() {
+        return leftRotationToTargetDegrees;
+    }
+
     abstract public void updateResult();
 
     abstract protected boolean resultIsValid();
@@ -53,8 +57,7 @@ abstract public class VisionSensor extends Component {
         telemetry.addData("Forward offset - forwardtarget: ", forwardDistanceToTargetInches - grabberForwardOffsetInches);
         newPose.setY(currentPose.getY() + leftDistanceToTargetInches - grabberLeftOffsetInches);
         telemetry.addData("left offset - lefttarget: ", leftDistanceToTargetInches - grabberLeftOffsetInches);
-
-        newPose.setHeading(currentPose.getHeading());
+        newPose.setHeading(Math.toDegrees(currentPose.getHeading()) + leftRotationToTargetDegrees);
 
         return (newPose);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/vision/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/vision/Webcam.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.common.vision;
 import android.util.Size;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.SortOrder;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -12,28 +13,32 @@ import org.firstinspires.ftc.vision.opencv.ColorRange;
 import org.firstinspires.ftc.vision.opencv.ImageRegion;
 import org.opencv.core.RotatedRect;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Webcam extends VisionSensor {
-    private ColorBlobLocatorProcessor colorLocator;
-    private VisionPortal portal;
+    private final ColorBlobLocatorProcessor colorLocator;
+    private final VisionPortal portal;
     private List<ColorBlobLocatorProcessor.Blob> blobs;
     private RotatedRect box;
-    private final int resWidth = 320;
-    private final int widthCenter = (int) (resWidth / 2.0);
+    private final int resolutionWidth = 320;
+    private final int widthCenter = (int) (resolutionWidth / 2.0);
     private int pixelsForwardOfCenter;
-    private double inchesForwardOfCenter;
-    private final int resHeight = 240;
-    private final int heightCenter = (int)(resHeight/2.0);
+    private final int resolutionHeight = 240;
+    private final int heightCenter = (int) (resolutionHeight / 2.0);
     private int pixelsLeftOfCenter;
-    private double inchesLeftOfCenter;
     double heightPixelsPerInch = 25;
     double widthPixelsPerInch = 25;
+    private RotatedRect box;
 
-    public Webcam(HardwareMap hardwareMap, Telemetry telemetry, String name) {
+    ColorBlobLocatorProcessor.BlobFilter areaFilter, densityFilter, ratioFilter;
+
+    ColorBlobLocatorProcessor.BlobSort areaSort, densitySort, ratioSort;
+
+    public Webcam(HardwareMap hardwareMap, Telemetry telemetry) {
         super(telemetry);
         grabberLeftOffsetInches = 0;
-        grabberForwardOffsetInches = 0;
+        grabberForwardOffsetInches = 3.5;
         forwardMinInches = -5;
         forwardMaxInches = 5;
         leftMinInches = -5;
@@ -42,17 +47,46 @@ public class Webcam extends VisionSensor {
         colorLocator = new ColorBlobLocatorProcessor.Builder()
                 .setTargetColorRange(ColorRange.YELLOW)         // use a predefined color match
                 .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
-                .setRoi(ImageRegion.asUnityCenterCoordinates(-1.0, 1.0, 1.0, -1.0))  // search central 1/4 of camera view
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-1.0, 1.0, 1.0, -1.0))
                 .setDrawContours(true)                        // Show contours on the Stream Preview
                 .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .setErodeSize(3)
+                .setDilateSize(3)
                 .build();
+
+        areaFilter = new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA, 100, 20000);
+        densityFilter = new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_DENSITY, 0.5, 1.0);
+        ratioFilter = new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_ASPECT_RATIO, 1.0, 10.0);
+
+        colorLocator.addFilter(areaFilter);
+        colorLocator.addFilter(densityFilter);
+        colorLocator.addFilter(ratioFilter);
+
+//        areaSort = new ColorBlobLocatorProcessor.BlobSort(ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA, SortOrder.ASCENDING);
+//        densitySort = new ColorBlobLocatorProcessor.BlobSort(ColorBlobLocatorProcessor.BlobCriteria.BY_DENSITY, SortOrder.ASCENDING);
+//        ratioSort = new ColorBlobLocatorProcessor.BlobSort(ColorBlobLocatorProcessor.BlobCriteria.BY_ASPECT_RATIO, SortOrder.ASCENDING);
+
+//        colorLocator.setSort(areaSort);
+//        colorLocator.setSort(densitySort);
+//        colorLocator.setSort(ratioSort);
 
         portal = new VisionPortal.Builder()
                 .addProcessor(colorLocator)
-                .setCameraResolution(new Size(resWidth, resHeight))
+                .setCameraResolution(new Size(resolutionWidth, resolutionHeight))
                 .setCamera(hardwareMap.get(WebcamName.class, "armcam"))
                 .build();
         start();
+    }
+
+    public RotatedRect getBestBox() {
+        updateFilteredResult(100);
+        return box;
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
     }
 
     public boolean updateFilteredResult(double maxSenseTimeMS) {
@@ -61,37 +95,43 @@ public class Webcam extends VisionSensor {
         while (((!resultIsValid()))
                 && (timer.milliseconds() < maxSenseTimeMS)) {
             updateResult();
-//            ColorBlobLocatorProcessor.Util.filterByArea(minArea, maxArea, blobs);
-//            ColorBlobLocatorProcessor.Util.filterByDensity(minDensity, maxDensity, blobs);
-//            ColorBlobLocatorProcessor.Util.filterByAspectRatio(minAspect, maxAspect, blobs);
         }
         if (resultIsValid()) {
+            Collections.sort(blobs, new BlobComparator());
             box = blobs.get(0).getBoxFit();
-            pixelsForwardOfCenter = heightCenter - (int)box.center.y;
-            pixelsLeftOfCenter =  widthCenter - (int) box.center.x;
-            forwardDistanceToTargetInches = pixelsForwardOfCenter/heightPixelsPerInch;
-            leftDistanceToTargetInches = pixelsLeftOfCenter/widthPixelsPerInch;
-            telemetry.addData("box center x: ",box.center.x);
-            telemetry.addData("box center y: ",box.center.y);
-            telemetry.addData("pixels forward of center: ",pixelsForwardOfCenter);
-            telemetry.addData("pixels left of center: ",pixelsLeftOfCenter);
+            pixelsForwardOfCenter = heightCenter - (int) box.center.y;
+            pixelsLeftOfCenter = widthCenter - (int) box.center.x;
+            forwardDistanceToTargetInches = pixelsForwardOfCenter / heightPixelsPerInch;
+            leftDistanceToTargetInches = pixelsLeftOfCenter / widthPixelsPerInch;
+            telemetry.addData("box center x: ", box.center.x);
+            telemetry.addData("box center y: ", box.center.y);
+            telemetry.addData("box angle: ", box.angle);
+
+            telemetry.addData("pixels forward of center: ", pixelsForwardOfCenter);
+            telemetry.addData("pixels left of center: ", pixelsLeftOfCenter);
             telemetry.update();
+
         }
         return (resultIsValid());
     }
 
-    public void updateResult() {
+    private void updateResult() {
         blobs = colorLocator.getBlobs();
     }
 
-    protected boolean resultIsValid() {
+    private boolean resultIsValid() {
         return (!blobs.isEmpty());
     }
 
-    public void start() {
+    private double distanceFromCenter(RotatedRect box) {
+        return (Math.abs(box.center.x - widthCenter) + Math.abs(box.center.y - heightCenter));
     }
 
-    public void stop() {
+    class BlobComparator implements java.util.Comparator<ColorBlobLocatorProcessor.Blob> {
+        @Override
+        public int compare(ColorBlobLocatorProcessor.Blob a, ColorBlobLocatorProcessor.Blob b) {
+            return (distanceFromCenter(a.getBoxFit()) - distanceFromCenter(b.getBoxFit()));
+        }
     }
 }
 
