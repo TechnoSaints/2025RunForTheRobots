f41313d6473b783938715b0e494a7f05a44ab599
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-06-10 15:05:13 -0400

    Create PushPathTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/PushPathTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.positions.IntakeSwivelPositions;
+import org.firstinspires.ftc.teamcode.opmode.Paths;
+
+@Autonomous(name = "BasicMove", group = "Bucket")
+public class PushPathTest extends BucketAutoOpMode {
+
+    protected void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+//                bot.followPath(Paths.startToBucket, true);
+//                bot.moveManualInches(-12,12,0);
+                bot.followPath(Paths.bucketToSampleSpike3, true);
+                setPathState(1);
+                break;
+
+            case 1:
+                if (!bot.isBusy()) {
+                    bot.setIntakeSwivelPositionPreset(IntakeSwivelPositions.DEGREES90);
+                    setPathState(2);
+                }
+                break;
+
+            // deactivate and rest up for teleop
+            case 2:
+                if (!bot.isBusy()) {
+                    setPathState(-1);
+                    requestOpModeStop();
+                }
+                break;
+        }
+    }
+}
+
